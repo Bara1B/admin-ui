cce2c00b6b96f5fec0d732a478223ba61708e275
Mbaraja <barajazkingrbg@gmail.com>
2024-11-04 16:55:15 +0700

    uts update

---
+++ src/component/Elements/CardC/index.jsx
@@ -0,0 +1,9 @@
+const CardC = ({ }) => {
+    return (
+        <div className="bg-white p-8 rounded-md shadow-lg transition-transform transform hover:scale-105 w-120 h-144">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt recusandae voluptatum pariatur voluptatem tempora eos dicta est sapiente nesciunt natus, sint dolore ea cum aut nostrum voluptate? Eos, velit a.
+        </div>
+    );
+};
+
+export default CardC;
--- src/pages/expencces.jsx
+++ src/pages/expencces.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import CardC from "../component/Elements/CardC";
+import CardD from "../component/Elements/CardD";
+import MainLayout from "../component/Layouts/MainLayout";
 
-const expencces = () => {
-  return <div>expencces</div>;
+const ExpenccesPage = () => {
+  return (
+    <MainLayout type="expencces">
+      <h1 className="text-gray-500 text-2xl mb-4">Expenses Comparison</h1>
+
+      <div className="mb-8 sm:flex sm:gap-6">
+        <CardD />
+      </div>
+
+      <h1 className="text-gray-500 text-2xl mb-4">Expenses Breakdown</h1>
+
+      <div className="mb-8 sm:flex sm:gap-6">
+        <CardC cardTitle="Credit Card" cardType="Master Card" logoSrc="/images/mc.png" accountNumber="1234 5678 9012 8***" totalAmount="$25,000" />
+        <CardC cardTitle="Checking" cardType="All Bank Ltd" logoSrc="/images/visa.png" accountNumber="693 456 69 9****" totalAmount="$25,000" />
+        <CardC cardTitle="Savings" cardType="Brsc Bank Ltd" accountNumber="133 456 886 8***" totalAmount="$25,000" /> {/* logoSrc not passed */}
+      </div>
+      <div className="mb-8 sm:flex sm:gap-6">
+        <CardC cardTitle="Credit Card" cardType="Master Card" logoSrc="/images/mc.png" accountNumber="1234 5678 9012 8***" totalAmount="$25,000" />
+        <CardC cardTitle="Checking" cardType="All Bank Ltd" logoSrc="/images/visa.png" accountNumber="693 456 69 9****" totalAmount="$25,000" />
+        <CardC cardTitle="Savings" cardType="Brsc Bank Ltd" accountNumber="133 456 886 8***" totalAmount="$25,000" /> {/* logoSrc not passed */}
+      </div>
+
+    </MainLayout>
+  );
 };
 
-export default expencces;
+export default ExpenccesPage;
